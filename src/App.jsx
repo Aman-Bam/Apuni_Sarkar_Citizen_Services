90123148aa20d3c9f37c350e921e1887e8629466
Aman-Bam <mhub7058@gmail.com>
2025-11-13 01:59:16 +0530

    gh

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Dashboard from './components/Dashboard'
 import './index.css'
-
+import { SpeedInsights } from "@vercel/speed-insights/react"
 function App() {
   return (
     <div className="min-h-screen bg-gray-50">
